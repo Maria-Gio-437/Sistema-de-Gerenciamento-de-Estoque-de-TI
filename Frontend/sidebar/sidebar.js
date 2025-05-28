998467ee51ef60073ec2230d0b3d223941c27466
Henrique <henrique.ruffato@academico.unirv.edu.br>
2025-05-28 20:36:10 -0300

    Mudança Logo e melhoria sidebar funcionamente pasta cadastro

--- Frontend/sidebar/sidebar.js
+++ Frontend/sidebar/sidebar.js
@@ -12,3 +12,15 @@ function carregarTela(event, caminho) {
       document.getElementById("conteudo").innerHTML = "<p>Erro ao carregar a tela.</p>";
     });
 }
+
+function toggleSubmenu(event) {
+  event.preventDefault();
+  const submenu = event.target.nextElementSibling; 
+  if (submenu && submenu.classList.contains('submenu')) {
+    if (submenu.style.display === 'none' || submenu.style.display === '') {
+      submenu.style.display = 'block';
+    } else {
+      submenu.style.display = 'none';
+    }
+  }
+}
